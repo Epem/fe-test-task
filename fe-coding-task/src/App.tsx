e5f329c85273612f8df315e63b56a65a0ef6bf65
catz85 <catz85@gmail.com>
2024-03-29 16:55:32 +0100

    fix: code cleanup

--- fe-coding-task/src/App.tsx
+++ fe-coding-task/src/App.tsx
@@ -3,11 +3,9 @@ import { Chart } from './components/Chart';
 import { DataTable } from './components/DataTable';
 import { Filters } from './components/Filters';
 import { FiltersForm } from './components/FormProvider';
-import { useGetChartDataMutation } from './state/api';
 import Paper from '@mui/material/Paper';
 
 function App() {
-  useGetChartDataMutation()
   return (
     <Paper sx={{ minWidth: 1200, margin: 'auto'}}>
       <FiltersForm>
